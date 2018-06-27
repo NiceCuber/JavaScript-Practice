adbc1d2a737b1c24885b8fc75b4a4f6ffd2e0bec
Giovanni Diaz <giovannidiaz1996@yahoo.com>
2018-06-27 12:54:27 -0400

    Add files via upload
    I just decided to have some fun with a bit of JavaScript I picked up from Codecademy. Nothing really special in my opinion but I'm glad to be putting my new knowledge to use.

---
+++ JavaScript Practice.js
@@ -0,0 +1,27 @@
+<h1>JavaScript Practice</h1>
+console.log("Capcom");
+console.log(13);
+console.log(true);
+console.log(null);
+console.log(9+4);
+console.log(6+7);
+console.log(12.3+0.7);
+console.log(9*4);
+console.log(40/20);
+console.log(27-7);
+console.log("Devil May Cry".length);
+console.log("Bento".toUpperCase());
+console.log("Bento".startsWith("B"));
+console.log(Math.random());
+console.log(Math.random()*140);
+console.log(Math.floor(Math.random()*90));
+console.log(Number.isInteger(2201));
+/*I have had a vision for many, many years! It has been
+for me to have a brand new chuurch . . . so can come here
+on Sunday . . . and preach the word of GAAAUUUDH!*/
+const myName="Dante";
+console.log(myName);
+const myAge=16;
+console.log(myAge);
+let meal="Pumpkin pie";
+console.log(meal);
